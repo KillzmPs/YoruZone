36fbeb2f3ee0ea20b531f4c8362b59e2d109dcbd
KillzmPs <gui.sou@ua.pt>
2026-05-26 14:34:00 +0100

    chore: update API_URL to ngrok tunnel

--- frontend/src/components/Historico.js
+++ frontend/src/components/Historico.js
@@ -1,4 +1,4 @@
-const API_URL = "https://yoru-zone-ivbq.vercel.app";
+const API_URL = "https://aghast-unsaved-untidy.ngrok-free.dev";
 
 const Historico = async (Id) => {
     try {
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,4 +1,4 @@
-const API_URL = "https://yoru-zone-ivbq.vercel.app";
+const API_URL = "https://aghast-unsaved-untidy.ngrok-free.dev";
 
 const LogIn = async (email, password) => {
     try {
